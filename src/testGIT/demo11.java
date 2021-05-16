00cf4791089f335015f66aee9f668d1ad96421c1
shantala <shantala12@gmail.com>
2021-05-16 13:15:30 -0400

    new update for code

--- src/testGIT/demo11.java
+++ src/testGIT/demo11.java
@@ -1,13 +1,10 @@
 package testGIT;
 
-public class demoGit {
+public class demo11 {
 
 	public static void main(String[] args) {
-	
-		
 		// TODO Auto-generated method stub
 
-		System.out.println("welcome to GIT");
 	}
 
 }
